6ff5103dc83dbcb1e070677d340725d835fe443d
Johan Pettersson <xsnudelx@hotmail.com>
2015-09-26 15:42:50 +0200

    Initial commit

---
+++ src/Server/Server.java
@@ -0,0 +1,12 @@
+package Server;
+
+/**
+ * Assignment 2A
+ * Created by Peonsson on 2015-09-26.
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
