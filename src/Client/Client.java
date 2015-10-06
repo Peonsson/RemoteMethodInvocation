94ec9853ea9dd41c7821ac303ff58f480a23674f
johanpettersson <xsnudelx@hotmail.com>
2015-10-06 17:07:30 +0200

    changed broadcast

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,22 +1,22 @@
 package Client;
 
-import Server.ChatServer;
 import Server.ChatServerInterface;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  * Assignment 2A
  * Created by Peonsson & roppe546 on 2015-09-26.
  */
-public class Client implements Notifiable {
+public class Client extends UnicastRemoteObject implements Notifiable {
 
     private ChatServerInterface chatServerInterface;
 
-    public Client(ChatServerInterface s) {
+    public Client(ChatServerInterface s) throws RemoteException {
         super();
         this.chatServerInterface = s;
     }
@@ -26,37 +26,17 @@ public class Client implements Notifiable {
         int i = 0;
 
        try {
-           System.out.println("test" + i++); //0
-
            String url = "rmi://localhost/chatserver";
-
-           System.out.println("test" + i++); //1
-
            ChatServerInterface chatServer = (ChatServerInterface) Naming.lookup(url);
-
-           System.out.println("test" + i++); //2
-
            Client c = new Client(chatServer);
-
-           System.out.println("test" + i++); //3
-
            chatServer.register(c);
-
-           System.out.println("test" + i++); //4
-
-           chatServer.broadcast();
-
-           System.out.println("test" + i++);
-
-       }
-       catch (NotBoundException e) {
-          e.printStackTrace();
-       }
-       catch (MalformedURLException e) {
-          e.printStackTrace();
-       }
-       catch (RemoteException e) {
-          e.printStackTrace();
+           chatServer.broadcast("Hello World!");
+       } catch (RemoteException e) {
+           e.printStackTrace();
+       } catch (MalformedURLException e) {
+           e.printStackTrace();
+       } catch (NotBoundException e) {
+           e.printStackTrace();
        }
     }
 
